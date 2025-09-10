1f38c7f77474b92a30eb70e29ecad2612827b343
Lorenzo <lorenzomatrullo04@gmail.com>
2025-09-10 17:54:19 +0200

    reworked the loading spinner overlay to be a new imgui windows and so consuming inputs

--- Onyx Launcher/src/core/overlay.cpp
+++ Onyx Launcher/src/core/overlay.cpp
@@ -2,16 +2,26 @@
 
 void RenderLoadingOverlay()
 {
-	const auto& wnd = GetCurrentWindow();
-	ImDrawList* dl = wnd->DrawList;
-	ImVec2 pos = wnd->Pos;
-	ImVec2 size = wnd->Size;
+	// Anchor overlay to the current window area
+	const auto& parent = GetCurrentWindow();
+	ImVec2 pos = parent->Pos;
+	ImVec2 size = parent->Size;
+
+	// Create a dedicated overlay window to reliably capture inputs
+	SetNextWindowPos(pos);
+	SetNextWindowSize(size);
+	ImGuiWindowFlags flags = ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoSavedSettings | ImGuiWindowFlags_NoBackground | ImGuiWindowFlags_NoNav;
+	Begin("##loading_overlay", nullptr, flags);
+
+	ImVec2 winPos = GetWindowPos();
+	ImVec2 winSize = GetWindowSize();
+	ImDrawList* dl = GetWindowDrawList();
 
 	// Dim background
-	dl->AddRectFilled(pos, pos + size, h->CO({ 0, 0, 0, 0.75f }));
+	dl->AddRectFilled(winPos, winPos + winSize, h->CO({ 0, 0, 0, 0.75f }));
 
 	// Spinner
-	ImVec2 center = pos + ImVec2(size.x * 0.5f, size.y * 0.5f - 10);
+	ImVec2 center = winPos + ImVec2(winSize.x * 0.5f, winSize.y * 0.5f - 10);
 	float t = (float)ImGui::GetTime();
 	float radius = 18.0f;
 	float start = t * 4.0f;
@@ -24,6 +34,10 @@ void RenderLoadingOverlay()
 	vec2 ts = h->CT("Signing in...");
 	ImVec2 tpos = center + ImVec2(-ts.x * 0.5f, radius + 12);
 	dl->AddText(tpos, h->CO(colors::White), "Signing in...");
+
+	// Block interactions behind the overlay by capturing mouse input
+	SetCursorPos(ImVec2(0, 0));
+	InvisibleButton("##overlay_blocker", winSize);
+
+	End();
 }
-
-
